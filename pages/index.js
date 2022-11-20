71a441d6936a2f42225b0ff9736fcd6df312e2d0
programando <jhonjamesmg@hotmail.com>
2022-11-19 20:23:42 -0500

    add data, add images and render products

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
-
+import ProductItem from '../components/ProductItem';
+import data from '../utils/data.js';
 export default function Home() {
   return (
-    <Layout>
-      <div className="text-4xl">Body page</div>
+    <Layout title="Home">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product}></ProductItem>
+        ))}
+      </div>
     </Layout>
   );
 }
--- project-info.md
+++ project-info.md
@@ -1,4 +1,14 @@
 # Nextjs ECommerce Tutorial For Beginners 2022 [Next.js, MongoDB & Tailwind]
 
-project-info.md
-project-info.md
+1. Create web site layout
+
+   1. create layout component
+   2. add header
+   3. add main section
+   4. add footer
+   5. add tailwind classes
+
+2. List products
+   1. add data.js
+   2. add images
+   3. render products
